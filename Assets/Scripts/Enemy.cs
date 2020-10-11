86877c34e07aff9a750afb5ae26071de5d6c3642
BlazerX88 <65087254+BlazerX88@users.noreply.github.com>
2020-10-12 00:05:06 +0530

    The game assetts and materials for the game

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudparticles;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bird bird1 = collision.collider.GetComponent<bird>();
+        if (bird1 != null)
+        {
+            Instantiate(_cloudparticles,transform.position,Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y > -0.5)
+        {
+            Instantiate(_cloudparticles, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/bird.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bird : MonoBehaviour
+{
+    private Vector3 _initialposition;
+    private bool _birdwasLaunched;
+    private float _timesittingaround;
+    [SerializeField] private float _launcher = 500;
+
+    private void Awake()
+    {
+        _initialposition = transform.position;
+    }
+    private void Update()
+    {
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, _initialposition);
+        if (_birdwasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1f)
+        {
+            _timesittingaround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 10 || transform.position.x < -10 || _timesittingaround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+        Vector2 directiontoinitialposition = _initialposition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directiontoinitialposition * _launcher);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdwasLaunched = true;
+        GetComponent<LineRenderer>().enabled = false;
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
